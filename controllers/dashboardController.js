9443b88771dc67cbc9808c33aee9f8a3de6cc02e
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-08-07 23:59:44 +0530

    Added YouTube video count logic in Admin panel dashboard.

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,17 +1,20 @@
 const Project = require("../models/Project");
 const Blog = require("../models/Blog");
 const Contact = require("../models/ContactMessage");
+const YouTube = require("../models/YouTubeVideo");
 
 const getDashboardStats = async (req, res) => {
   try {
     const projectsCount = await Project.countDocuments();
     const blogsCount = await Blog.countDocuments();
     const messagesCount = await Contact.countDocuments();
+    const youTubeCount = await YouTube.countDocuments();
 
     res.json({
       projectsCount,
       blogsCount,
       messagesCount,
+      youTubeCount,
     });
   } catch (error) {
     console.error("Error fetching dashboard stats:", error);
