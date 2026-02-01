5a7be7265518c1203edd7856a3917e71caa8161f
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2026-02-01 18:46:47 +0530

    Backend: Addad backend logic, API routes & server for freelanceing count.

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,6 +2,7 @@ const Project = require("../models/Project");
 const Blog = require("../models/Blog");
 const Contact = require("../models/ContactMessage");
 const YouTube = require("../models/YouTubeVideo");
+const Freelance = require("../models/FreelanceProject");
 
 const getDashboardStats = async (req, res) => {
   try {
@@ -9,12 +10,14 @@ const getDashboardStats = async (req, res) => {
     const blogsCount = await Blog.countDocuments();
     const messagesCount = await Contact.countDocuments();
     const youTubeCount = await YouTube.countDocuments();
+    const freelanceCount = await Freelance.countDocuments();
 
     res.json({
       projectsCount,
       blogsCount,
       messagesCount,
       youTubeCount,
+      freelanceCount,
     });
   } catch (error) {
     console.error("Error fetching dashboard stats:", error);
